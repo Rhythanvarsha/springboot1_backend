04c427fabebadbae4ccad18c17bac642bcadcd37
RHYTHAN VARSHA JANAKIRAM <152013507+Rhythanvarsha@users.noreply.github.com>
2025-07-16 09:45:42 +0530

    Add files via upload

---
+++ src/main/java/com/example/springbootfirst/config/CorsConfig.java
@@ -0,0 +1,21 @@
+//package com.example.springbootfirst.config;// CorsConfig.java
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.servlet.config.annotation.CorsRegistry;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//
+//@Configuration
+//public class CorsConfig implements WebMvcConfigurer {
+//
+//            @Override
+//            public void addCorsMappings(CorsRegistry registry) {
+//                registry.addMapping("/**")
+//                        .allowedOrigins("http://localhost:5173","https://springboot1-backend-1.onrender.com")
+//                        .allowedMethods("*")
+//                        .allowedHeaders("*")
+//                        .allowCredentials(true);
+//
+//            }
+//        };
+//
+//

a7e924b38c6684a213ed698639865bdb23399a8b
RHYTHAN VARSHA JANAKIRAM <152013507+Rhythanvarsha@users.noreply.github.com>
2025-07-16 09:23:10 +0530

    Update RolesController.java

--- src/main/java/com/example/springbootfirst/controllers/RolesController.java
+++ src/main/java/com/example/springbootfirst/controllers/RolesController.java
@@ -1,23 +1,23 @@
-// RoleController.java
-package com.example.springbootfirst.controllers;
+// // RoleController.java
+// package com.example.springbootfirst.controllers;
 
-import com.example.springbootfirst.models.Roles;
-import com.example.springbootfirst.repository.RolesRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+// import com.example.springbootfirst.models.Roles;
+// import com.example.springbootfirst.repository.RolesRepository;
+// import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
+// import java.util.List;
 
-@CrossOrigin(origins = "http://localhost:5173")
-@RestController
-@RequestMapping("/api/auth")// Enable CORS for frontend
-public class RolesController {
+// @CrossOrigin(origins = "http://localhost:5173")
+// @RestController
+// @RequestMapping("/api/auth")// Enable CORS for frontend
+// public class RolesController {
 
-    @Autowired
-    private RolesRepository rolesRepository;
+//     @Autowired
+//     private RolesRepository rolesRepository;
 
-    @GetMapping("/roles")
-    public List<Roles> getAllRoles() {
-        return rolesRepository.findAll();
-    }
-}
+//     @GetMapping("/roles")
+//     public List<Roles> getAllRoles() {
+//         return rolesRepository.findAll();
+//     }
+// }

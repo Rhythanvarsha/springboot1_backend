ac307c5918de5d0f913c1cd2039b392d86f63a67
RHYTHAN VARSHA JANAKIRAM <152013507+Rhythanvarsha@users.noreply.github.com>
2025-07-18 10:30:59 +0530

    Update RegisterDetails.java

--- src/main/java/com/example/springbootfirst/models/RegisterDetails.java
+++ src/main/java/com/example/springbootfirst/models/RegisterDetails.java
@@ -11,34 +11,34 @@ import java.util.Set;
 
 
 @Data
-    @AllArgsConstructor // Generates constructor with all fields
-    @NoArgsConstructor // Generates constructor with no filed
-    @Entity
-    @Table(name = "user_details")
-    public class RegisterDetails {
-        @Id
-        @GeneratedValue(strategy = GenerationType.IDENTITY)
-        private int empId;
+@AllArgsConstructor // Generates constructor with all fields
+@NoArgsConstructor // Generates constructor with no filed
+@Entity
+@Table(name = "user_details")
+public class RegisterDetails {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int empId;
 
-        @Column(name = "emp_name" ,nullable=false)//to get mandatory
-        private String empName;
+    @Column(name = "emp_name" ,nullable=false)//to get mandatory
+    private String empName;
 
-        @Column(nullable = false,unique = true )
-        private String email;
+    @Column(nullable = false,unique = true )
+    private String email;
 
-       @Column(unique = true)
-        private String password;
+    @Column(unique = true)
+    private String password;
 
-        @Column(name="user_name",nullable=false,unique=true)
-        private String userName;
+    @Column(name="user_name",nullable=false,unique=true)
+    private String userName;
 
-        @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
-        @JoinTable(
-                name = "user_roles",
-                joinColumns = @JoinColumn(name = "user_id", referencedColumnName = "empId"),
-                inverseJoinColumns = @JoinColumn(name = "role_id", referencedColumnName = "roleId")
-        )
-        private Set<Roles> roles;
+    @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    @JoinTable(
+            name = "user_roles",
+            joinColumns = @JoinColumn(name = "user_id", referencedColumnName = "empId"),
+            inverseJoinColumns = @JoinColumn(name = "role_id", referencedColumnName = "roleId")
+    )
+    private Set<Roles> roles;
 
 
 
